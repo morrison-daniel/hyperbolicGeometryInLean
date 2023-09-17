6700ae7e828593d128682688e6b4bc5e615fe933
morrison-daniel <39346894+morrison-daniel@users.noreply.github.com>
2023-09-16 22:44:39 -0700

    start minkowski space

--- Hyperbolic/Minkowski.lean
+++ Hyperbolic/Minkowski.lean
@@ -1,30 +1,56 @@
 import Hyperbolic.PseudoEuclideanSpace
+import Hyperbolic.Arcosh
 
 open BigOperators ComplexConjugate PseudoInnerProductSpace PseudoEuclideanSpace
 
-variable {d : ℕ}
-
-def MinkowskiSpace : PseudoEuclideanSpace ℝ (Fin d) where
+def MinkowskiSpace (d : ℕ+) : PseudoEuclideanSpace ℝ (Fin d) where
   signature := fun i => if (i : ℕ) = 0 then Sign.Minus else Sign.Plus
 
-noncomputable instance : PseudoInnerProductSpace ℝ (Fin d → ℝ) :=
-  instPseudoInnerProductSpaceofPseudoEuclideanSpace MinkowskiSpace
+noncomputable instance (d : ℕ+) : PseudoInnerProductSpace ℝ (Fin d → ℝ) :=
+  instPseudoInnerProductSpaceofPseudoEuclideanSpace (MinkowskiSpace d)
 
-local notation "E" => Fin d → ℝ
-local notation "⟪" x ", " y "⟫" => @inner ℝ E _ x y
+variable {d : ℕ+}
 
-def TimeLike (v : E)      : Prop := ⟪v, v⟫ <  0
-def UnitTimeLike (v : E)  : Prop := ⟪v, v⟫ = -1
-def LightLike (v : E)     : Prop := ⟪v, v⟫ =  0
-def SpaceLike (v : E)     : Prop := ⟪v, v⟫ >  0
-def UnitSpaceLike (v : E) : Prop := ⟪v, v⟫ =  1
+local notation "M" => Fin d → ℝ
+local notation "⟪" x ", " y "⟫" => @inner ℝ _ _ x y
 
-theorem tl_of_utl {v : E} (h : UnitTimeLike v) : TimeLike v := by
+def TimeLike (v : M)      : Prop := ⟪v, v⟫ <  0
+def UnitTimeLike (v : M)  : Prop := ⟪v, v⟫ = -1
+def LightLike (v : M)     : Prop := ⟪v, v⟫ =  0
+def SpaceLike (v : M)     : Prop := ⟪v, v⟫ >  0
+def UnitSpaceLike (v : M) : Prop := ⟪v, v⟫ =  1
+
+def Future (v : M) : Prop := v 0 > 0
+
+def Hyperboloid (v : M) : Prop := UnitTimeLike v ∧ Future v
+
+noncomputable def hdist (v w : M) {_ : Hyperboloid v} {_ : Hyperboloid w} : M → M → ℝ :=
+  fun v w => Real.arcosh (- ⟪v, w⟫)
+
+namespace MinkowskiSpace
+
+lemma inner_symm {x y : M} : ⟪x, y⟫ = ⟪y, x⟫ := by
+  rw [← PseudoInnerProductSpace.inner_conj_symm x y]
+  rfl
+
+theorem tl_of_utl (v : M) {h : UnitTimeLike v} : TimeLike v := by
   rw [UnitTimeLike] at h
   rw [TimeLike, h]
   exact neg_one_lt_zero
 
-theorem sl_of_usl {v : E} (h : UnitSpaceLike v) : SpaceLike v := by
+theorem sl_of_usl (v : M) {h : UnitSpaceLike v} : SpaceLike v := by
   rw [UnitSpaceLike] at h
   rw [SpaceLike, h]
   exact zero_lt_one
+
+def origin : M := fun i => if (i : ℕ) = 0 then 1 else 0
+
+end MinkowskiSpace
+
+namespace MinkowskiFour
+
+local notation "M4" => Fin 4 → ℝ
+
+
+
+end MinkowskiFour
